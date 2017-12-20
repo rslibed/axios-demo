9e44ae12d039e86c743dcb57ccd4bd38726fae46
rslibed <rslibed@gmail.com>
2017-12-20 12:47:23 -0800

    Covered different types of requests to get data from servers

--- script.js
+++ script.js
@@ -1,47 +1,35 @@
-var BASE_URL = 'http://api.reactprototypes.com';
-var API_KEY = '?key=testuser1234';
+console.log('script file loaded.');
+const BASE_URL = 'http://s-apis.learningfuze.com/sgt/get';
+const API_KEY = '?key=testuser1234';
+const dataToSend = 'api_key=d7r3OiW4rR';
 
-axios.get(BASE_URL + '/todos' + API_KEY).then(function(resp){
-    var todos = resp.data.todos;
-    var table = $('table tbody');
-
-    // addToDom(todos, table);
-    addToDomNoMap(todos, table);
-});
-
-var addToDom = function(list, container) {
-    console.info('Using Map');
-
-    var tableRows = list.map((item, index) => {
-
-        var tableData = [
-            $(`<td>${index + 1}</td>`),
-            $(`<td>${item.title}</td>`),
-            item.complete ? $(`<td class="text-success">Yes</td>`) : $(`<td class="text-danger">No</td>`)
-        ];
-
-        return $('<tr>').append(tableData);
-    });
-
-    container.append(tableRows);
-};
-
-var addToDomNoMap = function(list, container) {
-    console.info('Using For Loop');
-
-    var tableRows = [];
-
-    for(var i = 0; i < list.length; i++){
-        var item = list[i];
-
-        var tableData = [
-            $(`<td>${i + 1}</td>`),
-            $(`<td>${item.title}</td>`),
-            item.complete ? $(`<td class="text-success">Yes</td>`) : $(`<td class="text-danger">No</td>`)
-        ];
-
-        tableRows.push($('<tr>').append(tableData));
+const settings = {
+    headers: {
+        'content-type': 'application/x-www-form-urlencoded'
     }
-
-    container.append(tableRows);
 };
+
+// // GET METHOD FOR AXIOS
+// axios.get(`${BASE_URL}/todos${API_KEY}`).then( (response) => {
+//     console.log(response.data);
+// });
+
+
+// // POST METHOD FOR AXIOS
+// axios.post(BASE_URL, dataToSend, settings)
+//     .then(handleResponse)
+//     .catch( (err) => {
+//         console.log("Error: ", err);
+//     })
+//
+// function handleResponse(response) {
+//     console.log("Real function server response: ", response.data);
+// }
+
+//
+// // ES6 way of doing AJAX calls
+// fetch('http://api.reactprototypes.com/todos?key=testuser1234').then(resp=> {
+//     return resp.json();
+// }).then(resp => {
+//     console.log("Resp: ", resp);
+// })
